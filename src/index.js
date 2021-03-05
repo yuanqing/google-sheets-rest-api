33089278677adfaf550f3808c420f39f679351a5
yuanqing <desk@yuanqing.sg>
2021-03-05 08:42:12 +0800

    Refactor

--- src/index.js
+++ src/index.js
@@ -1,46 +1,49 @@
 ;(function (window) {
-  function read (options) {
-    return post(options.deploymentId, {
-      operation: 'read',
-      sheetName: options.sheetName
-    })
-  }
-
-  function add (data, options) {
-    return post(options.deploymentId, {
-      operation: 'add',
-      sheetName: options.sheetName,
-      accessToken: options.accessToken,
-      mode: typeof options.mode === 'undefined' ? 'append' : options.mode,
-      data
-    })
-  }
-
-  async function post (deploymentId, body) {
-    const result = await window.fetch(
-      `https://script.google.com/macros/s/${deploymentId}/exec`,
-      {
-        method: 'post',
-        body: JSON.stringify(body),
-        headers: {
-          'Content-Type': 'text/plain; charset=UTF-8'
-        }
-      }
-    )
-    const json = await result.json()
-    if (json.error !== null) {
-      throw new Error(json.error)
+  function googleSheets (config) {
+    function read (options) {
+      return post({
+        operation: 'read',
+        sheetName: options.sheetName
+      })
     }
-    return json.result
+
+    function add (data, options) {
+      return post({
+        operation: 'add',
+        sheetName: options.sheetName,
+        accessToken: config.accessToken,
+        mode: typeof options.mode === 'undefined' ? 'append' : options.mode,
+        data
+      })
+    }
+
+    async function post (body) {
+      const result = await window.fetch(
+        `https://script.google.com/macros/s/${config.deploymentId}/exec`,
+        {
+          method: 'post',
+          body: JSON.stringify(body),
+          headers: {
+            'Content-Type': 'text/plain; charset=UTF-8'
+          }
+        }
+      )
+      const json = await result.json()
+      if (json.error !== null) {
+        throw new Error(json.error)
+      }
+      return json.result
+    }
+
+    return {
+      add,
+      read
+    }
   }
 
-  const sheets = {
-    add,
-    read
-  }
   if (typeof module === 'object') {
-    module.exports = sheets
+    module.exports = googleSheets
   } else {
-    window.sheets = sheets
+    window.googleSheets = googleSheets
   }
 })(this)
